8bc44040cd03f68daba644616f0e5dddff47f566
Aleix Pol <aleixpol@kde.org>
2016-09-22 17:43:48 +0200

    Print better

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <QCoreApplication>
 #include <QTextStream>
 #include <QDebug>
+#include <QMetaEnum>
 #include <AppstreamQt/database.h>
 
 int main(int argc, char **argv)
 {
     QCoreApplication app(argc, argv);
 
-//     QTextStream cout(stdout, QIODevice::WriteOnly);
+    QTextStream cout(stdout, QIODevice::WriteOnly);
     QTextStream cerr(stderr, QIODevice::WriteOnly);
 
     Appstream::Database db;
@@ -16,8 +17,19 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    const auto compo = db.componentById(app.arguments().first());
-    qDebug() << "name: " << compo.name() << '\n';
-    qDebug() << "homepage: " << compo.urls();
+    const auto compo = db.componentById(app.arguments().last());
+    if (!compo.isValid()) {
+        cerr << "couldn't find " << app.arguments().last() << '\n';
+        return 2;
+    }
+    cout << "name: " << compo.name() << '\n';
+    cout << "urls:\n";
+
+    const auto enumerator = Appstream::Component::staticMetaObject.enumerator(Appstream::Component::staticMetaObject.indexOfEnumerator("UrlKind"));
+
+    auto urls = compo.urls();
+    for(auto it = urls.constBegin(); it != urls.constEnd(); ++it) {
+        cout << it.key() << ' ' << enumerator.valueToKey(it.key()) << ": " << it.value().toDisplayString() << '\n';
+    }
     return 0;
 }
